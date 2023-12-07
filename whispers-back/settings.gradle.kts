7a7fed0f78d4b6190d596c61c28487eff714da8c
Samuel Grigolato <samuel.grigolato@gmail.com>
2023-12-06 22:42:53 -0300

    Fix weird gradle settings for whispers-back

--- whispers-back/settings.gradle.kts
+++ whispers-back/settings.gradle.kts
@@ -2,8 +2,6 @@ rootProject.name = "whispers-back"
 include("whispers-back-domain")
 include("whispers-back-app")
 include("whispers-back-api")
-include("whispers-back-api:whispers-back-jpa")
-findProject(":whispers-back-api:whispers-back-jpa")?.name = "whispers-back-jpa"
 include("whispers-back-jpa")
 include("whispers-back-server1")
 include("whispers-back-dynamo")
