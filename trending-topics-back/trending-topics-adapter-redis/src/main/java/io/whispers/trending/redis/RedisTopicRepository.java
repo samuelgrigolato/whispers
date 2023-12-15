fe449e79ff9f10b67e8465150d00401df2686001
Samuel Grigolato <samuel.grigolato@gmail.com>
2023-12-15 19:23:00 -0300

    Remove current implementation

--- trending-topics-back/trending-topics-adapter-redis/src/main/java/io/whispers/trending/redis/RedisTopicRepository.java
+++ trending-topics-back/trending-topics-adapter-redis/src/main/java/io/whispers/trending/redis/RedisTopicRepository.java
@@ -25,30 +25,6 @@ public class RedisTopicRepository implements TrendingTopicRepository {
 
     @Override
     public void incrementWhisperCounts(Map<String, Integer> topicIncrements) {
-        String minuteMapKey = getMinuteMapKey(ZonedDateTime.now(ZoneId.of("Etc/UTC")));
-        var bucket = (int)(Math.random() * BUCKETS);
-        RLock lock = this.redisson.getLock("topicsLock-" + bucket);
-        try {
-            lock.lockInterruptibly(5, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            throw new RuntimeException("Unable to acquire lock.", e);
-        }
-        try {
-            var map = this.redisson.<String, Map<String, Integer>>getMap("topics-" + bucket);
-            for (var entry : topicIncrements.entrySet()) {
-                var topic = entry.getKey();
-                var increment = entry.getValue();
-                var minuteMap = map.get(minuteMapKey);
-                if (minuteMap == null) {
-                    minuteMap = new HashMap<>();
-                }
-                minuteMap.putIfAbsent(topic, 0);
-                minuteMap.put(topic, minuteMap.get(topic) + increment);
-                map.put(minuteMapKey, minuteMap); // this is necessary even if the map already exists!
-            }
-        } finally {
-            lock.unlock();
-        }
     }
 
     @Override
@@ -56,24 +32,22 @@ public class RedisTopicRepository implements TrendingTopicRepository {
         var lastMinutesMapKeys = getLastMinutesMapKeys();
         var aggregatedWhisperCounts = new HashMap<String, Integer>();
 
-        for (var bucket = 0; bucket < BUCKETS; bucket++) {
-            var map = this.redisson.<String, Map<String, Integer>>getMap("topics-" + bucket);
-            for (var minuteMapKey : lastMinutesMapKeys) {
-                var minuteMap = map.get(minuteMapKey);
-                if (minuteMap != null) {
-                    for (var entry : minuteMap.entrySet()) {
-                        var topic = entry.getKey();
-                        var whisperCount = entry.getValue();
-                        aggregatedWhisperCounts.putIfAbsent(topic, 0);
-                        aggregatedWhisperCounts.put(topic, aggregatedWhisperCounts.get(topic) + whisperCount);
-                    }
+        var map = this.redisson.<String, Map<String, Integer>>getMap("topics");
+        for (var minuteMapKey : lastMinutesMapKeys) {
+            var minuteMap = map.get(minuteMapKey);
+            if (minuteMap != null) {
+                for (var entry : minuteMap.entrySet()) {
+                    var topic = entry.getKey();
+                    var whisperCount = entry.getValue();
+                    aggregatedWhisperCounts.putIfAbsent(topic, 0);
+                    aggregatedWhisperCounts.put(topic, aggregatedWhisperCounts.get(topic) + whisperCount);
                 }
             }
-            // older keys cleanup (older than the required minutes)
-            map.keySet().stream()
-                    .filter(key -> !lastMinutesMapKeys.contains(key))
-                    .forEach(map::fastRemove);
         }
+        // older keys cleanup (older than the required minutes)
+        map.keySet().stream()
+                .filter(key -> !lastMinutesMapKeys.contains(key))
+                .forEach(map::fastRemove);
 
         return aggregatedWhisperCounts.entrySet().stream()
                 .sorted(comparingByValue(Comparator.reverseOrder()))
