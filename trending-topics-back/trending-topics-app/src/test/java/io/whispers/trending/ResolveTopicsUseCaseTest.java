ed65e8dc30e0ecddc8414208d9116b53b97c00e9
Samuel Grigolato <samuel.grigolato@gmail.com>
2023-12-14 22:14:28 -0300

    Fix topic resolution use case test

--- trending-topics-back/trending-topics-app/src/test/java/io/whispers/trending/ResolveTopicsUseCaseTest.java
+++ trending-topics-back/trending-topics-app/src/test/java/io/whispers/trending/ResolveTopicsUseCaseTest.java
@@ -3,6 +3,7 @@ package io.whispers.trending;
 import io.whispers.trending.app.resolvetopics.ResolveTopicsUseCase;
 import io.whispers.trending.domain.*;
 import io.whispers.trending.domain.event.TopicResolvedEvent;
+import io.whispers.trending.domain.event.TopicResolvedEventPublisher;
 import io.whispers.trending.domain.event.WhisperCreatedEvent;
 import io.whispers.trending.domain.TrendingTopicRepository;
 import org.junit.jupiter.api.Test;
@@ -20,11 +21,11 @@ class ResolveTopicsUseCaseTest {
     void testNoopIfEmptyList() {
         var topicRepositoryMock = mock(TrendingTopicRepository.class);
         var topicResolverMock = mock(TopicResolver.class);
-        var topicEventPublisherMock = mock(TopicEventPublisher.class);
-        var subject = new ResolveTopicsUseCase(topicRepositoryMock, topicResolverMock, topicEventPublisherMock);
+        var topicResolvedEventPublisherMock = mock(TopicResolvedEventPublisher.class);
+        var subject = new ResolveTopicsUseCase(topicRepositoryMock, topicResolverMock, topicResolvedEventPublisherMock);
         subject.execute(List.of());
         verify(topicResolverMock, never()).resolveTopic(any());
-        verify(topicEventPublisherMock, never()).publishBatch(any());
+        verify(topicResolvedEventPublisherMock, never()).publishBatch(any());
         verify(topicRepositoryMock, never()).incrementWhisperCounts(any());
     }
 
@@ -32,14 +33,14 @@ class ResolveTopicsUseCaseTest {
     void testResolvingTopic() {
         var topicRepositoryMock = mock(TrendingTopicRepository.class);
         var topicResolverMock = mock(TopicResolver.class);
-        var topicEventPublisherMock = mock(TopicEventPublisher.class);
+        var topicResolvedEventPublisherMock = mock(TopicResolvedEventPublisher.class);
         when(topicResolverMock.resolveTopic("text")).thenReturn(Optional.of("topic"));
-        var subject = new ResolveTopicsUseCase(topicRepositoryMock, topicResolverMock, topicEventPublisherMock);
+        var subject = new ResolveTopicsUseCase(topicRepositoryMock, topicResolverMock, topicResolvedEventPublisherMock);
         subject.execute(List.of(new WhisperCreatedEvent(
                 UUID.fromString("00f8da7a-5799-48f3-88cc-b96a916712b1"),
                 "text"
         )));
-        verify(topicEventPublisherMock).publishBatch(List.of(new TopicResolvedEvent(
+        verify(topicResolvedEventPublisherMock).publishBatch(List.of(new TopicResolvedEvent(
                 UUID.fromString("00f8da7a-5799-48f3-88cc-b96a916712b1"),
                 "topic"
         )));
@@ -52,14 +53,14 @@ class ResolveTopicsUseCaseTest {
     void testNotResolvingTopic() {
         var topicRepositoryMock = mock(TrendingTopicRepository.class);
         var topicResolverMock = mock(TopicResolver.class);
-        var topicEventPublisherMock = mock(TopicEventPublisher.class);
+        var topicResolvedEventPublisher = mock(TopicResolvedEventPublisher.class);
         when(topicResolverMock.resolveTopic("text")).thenReturn(Optional.empty());
-        var subject = new ResolveTopicsUseCase(topicRepositoryMock, topicResolverMock, topicEventPublisherMock);
+        var subject = new ResolveTopicsUseCase(topicRepositoryMock, topicResolverMock, topicResolvedEventPublisher);
         subject.execute(List.of(new WhisperCreatedEvent(
                 UUID.fromString("00f8da7a-5799-48f3-88cc-b96a916712b1"),
                 "text"
         )));
-        verify(topicEventPublisherMock, never()).publishBatch(any());
+        verify(topicResolvedEventPublisher, never()).publishBatch(any());
         verify(topicRepositoryMock, never()).incrementWhisperCounts(any());
     }
 
